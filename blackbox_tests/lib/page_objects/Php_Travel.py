3e17180c8004fb5ad290271ca42cc0dbffeaec56
Mykola Shumakov <mshumako@cisco.com>
2019-09-25 11:46:28 +0300

    Added some fixes and looks like first test is working now))

---
+++ blackbox_tests/lib/page_objects/Php_Travel.py
@@ -0,0 +1,24 @@
+import random
+import time
+
+from selenium.webdriver.common.keys import Keys
+
+from blackbox_tests.lib.locators.php_travel import PhpTravelLocator
+from blackbox_tests.lib.page_objects.base_page import BasePage
+from blackbox_tests.lib.utils.constants import URL, Constants
+
+
+class TwitterTimeline(BasePage):
+    locator_dictionary = PhpTravelLocator.__dict__
+
+    def __init__(self, context):
+        BasePage.__init__(self,
+                          context.browser,
+                          )
+        self.context = context
+        self.base_url = URL.PHP_TRAVELS
+        self.visit(url=self.base_url)
+
+    def search(self, q=Constants.GLOBAL_ENTRY_Q):
+        self.search_input.send_keys(q)
+        self.search_input.send_keys(Keys.ENTER)
